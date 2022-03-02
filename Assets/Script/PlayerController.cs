b69fe09abc2312d23aa9a3cd430d7cb3b6ca1da3
Umesh Lahudkar <umeshlahudkar@gmail.com>
2022-03-02 22:32:39 +0530

    commit2
    Added script to play animation

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+   
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horrizontalInput = Input.GetAxisRaw("Horizontal");
+        float VerticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector2 scale = gameObject.transform.localScale;
+        if(horrizontalInput > 0)
+        {
+            scale.x = 1f * Mathf.Abs(scale.x);
+        }
+        if(horrizontalInput < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        gameObject.transform.localScale = scale;
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", false);
+        }
+
+        if(VerticalInput > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+    }
+}
